9ac2f049b01b6bb1f431bd03dc4da9aac47a9274
Shehanka <hcsperera@outlook.com>
2021-09-12 21:44:26 +0530

    User Other Routes Added

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-
+import * as mongodb from "mongodb";
 import { MongoHelper } from "../../config/mongodb.config";
 import User from "./user.class";
 import Config from "../../config/config";
@@ -26,6 +26,84 @@ export default class UserController {
         .status(httpStatus.CREATED)
         .send(responses.success(SuccessCodes.SUCCESSFULLY_DATA_ADDED));
       res.end();
+    } catch (e) {
+      console.error(e.message);
+      res.send(
+        responses.failed(ErrorCodes.INTERNAL_ERROR, httpStatus.BAD_REQUEST)
+      );
+    }
+  };
+
+  public getUser = async (req: Request, res: Response): Promise<any> => {
+    const collection = getCollection();
+
+    try {
+      console.log(req.params.id);
+      const user = await collection.findOne({
+        _id: req.params.id,
+      });
+
+      res.send(
+        responses.successWithPayload(
+          SuccessCodes.SUCCESSFULLY_DATA_RETRIVED,
+          user
+        )
+      );
+    } catch (e) {
+      console.error(e.message);
+      res.send(
+        responses.failed(ErrorCodes.INTERNAL_ERROR, httpStatus.BAD_REQUEST)
+      );
+    }
+  };
+
+  public getAllUsers = async (req: Request, res: Response): Promise<any> => {
+    const collection: any = getCollection();
+
+    try {
+      const data = await collection.find().toArray();
+
+      res.send(
+        responses.successWithPayload(
+          SuccessCodes.SUCCESSFULLY_DATA_RETRIVED,
+          data
+        )
+      );
+    } catch (e) {
+      console.error(e.message);
+      res.send(
+        responses.failed(ErrorCodes.INTERNAL_ERROR, httpStatus.BAD_REQUEST)
+      );
+    }
+  };
+
+  public updateUser = async (req: Request, res: Response): Promise<any> => {
+    const { _id, displayName, email, photoURL } = req.body;
+
+    const collection: any = getCollection();
+
+    try {
+      await collection.findOneAndUpdate(
+        { _id: new mongodb.ObjectId(_id) },
+        { $set: { displayName, email, photoURL } }
+      );
+
+      res.send(responses.success(SuccessCodes.SUCCESSFULLY_DATA_UPDATED));
+    } catch (e) {
+      console.error(e.message);
+      res.send(
+        responses.failed(ErrorCodes.INTERNAL_ERROR, httpStatus.BAD_REQUEST)
+      );
+    }
+  };
+
+  public deleteUser = async (req: Request, res: Response): Promise<any> => {
+    const id = req.params.id;
+    const collection: any = getCollection();
+
+    try {
+      await collection.deleteOne({ _id: new mongodb.ObjectId(id) });
+      res.send(responses.success(SuccessCodes.SUCCESSFULLY_DATA_DELETED));
     } catch (e) {
       console.error(e.message);
       res.send(

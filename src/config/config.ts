ec0731dcb2b145b61a88bce35ff890ccf3e6bc52
Shehanka <hcsperera@outlook.com>
2021-12-26 20:23:55 +0530

    DB URL Formatted

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,13 +3,17 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const MONGO_DB_URI = process.env.MONGO_DB_URI;
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const MONGO_DB_URI = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
 export default {
   PORT: PORT,
   MONGO_DB_URI: MONGO_DB_URI,
-  DB_NAME: "CodeKitty",
+  DB_NAME: DB_NAME,
   USER_COLLECTION_NAME: "user",
   CATEGORY_COLLECTION_NAME: "category",
-  POST_COLLECTION_NAME: "post"
+  POST_COLLECTION_NAME: "post",
 };
